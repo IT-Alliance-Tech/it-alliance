4758aaf2d5d5506d374f768c2e54a829eff69782
Gireeshma27 <gireeshma053@gmail.com>
2026-02-18 19:00:01 +0530

    updated trust section

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     '@tailwindcss/postcss': {},
+=======
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+>>>>>>> 33bf272 (Initial commit from Create Next App)
   },
 };
 
